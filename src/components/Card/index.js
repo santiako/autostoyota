c5fe4966688ab645549e93482a0c53dc3692264d
MacbookSanti <mac@SantiMac.local>
2020-05-12 12:04:43 -0300

    comit de prueba

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -20,6 +20,7 @@ function Card({ restaurant, onClick }) {
   const { id, cover } = restaurant;
   const coverURL = cover[0] ? cover[0].url : '';
 
+// lalal
   return (
     <StyledCard onClick={() => onClick(id)} className="clickable-card">
       <CardSection restaurant={restaurant} />
