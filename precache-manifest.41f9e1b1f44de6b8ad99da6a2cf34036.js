cfb23ac68b22f187e478a7a2b2c68a055c313387
Santiako <santiagoinsua@outlook.com>
2020-06-01 18:26:52 -0300

    Updates

---
+++ precache-manifest.41f9e1b1f44de6b8ad99da6a2cf34036.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "84fb968e7f05df0acf20b8acf4e47903",
+    "url": "/autostoyota/static/media/logo.84fb968e.svg"
+  },
+  {
+    "revision": "528183f503f1a85a0f06",
+    "url": "/autostoyota/static/js/main.d0b43e31.chunk.js"
+  },
+  {
+    "revision": "5290ee2ffdddc9298dfa58f6a21826a8",
+    "url": "/autostoyota/static/media/dots.5290ee2f.svg"
+  },
+  {
+    "revision": "75928c68e62bded1d1e414fde3541582",
+    "url": "/autostoyota/static/media/ooops.75928c68.png"
+  },
+  {
+    "revision": "78fd42129105e7deba172a3c30b18231",
+    "url": "/autostoyota/static/media/flechader.78fd4212.svg"
+  },
+  {
+    "revision": "9c132874a615f168a420",
+    "url": "/autostoyota/static/js/runtime~main.3c70b782.js"
+  },
+  {
+    "revision": "335f6361eb5cdc3a0a5565d15ae5f351",
+    "url": "/autostoyota/static/media/icon_star_active.335f6361.svg"
+  },
+  {
+    "revision": "362e83965543924eb2ceb3c63a7fa3cd",
+    "url": "/autostoyota/static/media/icon_star_inactive.362e8396.svg"
+  },
+  {
+    "revision": "da85e54998493987e94b",
+    "url": "/autostoyota/static/js/2.26b20bd5.chunk.js"
+  },
+  {
+    "revision": "da85e54998493987e94b",
+    "url": "/autostoyota/static/css/2.275d0061.chunk.css"
+  },
+  {
+    "revision": "7c25129f1c96a021d2abcb2fc1e2248a",
+    "url": "/autostoyota/index.html"
+  }
+];
